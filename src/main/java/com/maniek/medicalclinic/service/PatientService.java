18051152c44304903bac6fb5d93d4355d1858d55
maniuuu7 <maciejrajczyk1@gmail.com>
2023-02-22 12:54:57 +0100

    + MED-06

--- src/main/java/com/maniek/medicalclinic/service/PatientService.java
+++ src/main/java/com/maniek/medicalclinic/service/PatientService.java
@@ -1,73 +1,41 @@
 package com.maniek.medicalclinic.service;
 
 import com.maniek.medicalclinic.model.Patient;
+import com.maniek.medicalclinic.repository.PatientRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@AllArgsConstructor
 @Service
 public class PatientService {
-    private List<Patient> patients = new ArrayList<>();
+    private PatientRepository patientRepository;
+
 
     public List<Patient> showPatient() {
-        return patients;
+        return patientRepository.getAllPatients();
     }
 
     public Optional<Patient> getPatientByEmail(String email) {
-        return patients.stream()
-                .filter(patient1 -> patient1.getEmail().equals(email))
-                .findFirst();
+        return patientRepository.getPatientByEmail(email);
     }
 
     public Optional<Patient> addPatient(Patient patient) {
-        Optional<Patient> patient1 = patients.stream()
-                .filter(patient2 -> patient2.getEmail().equals(patient.getEmail()))
-                .findFirst();
-        if (patient1.isPresent()) {
-            return Optional.empty();
-        }
-        patients.add(patient);
-        return Optional.of(patient);
+        return patientRepository.addPatient(patient);
     }
 
     public Optional<Patient> deletePatientByEmail(String email) {
-        Optional<Patient> patient = patients.stream()
-                .filter(patient1 -> patient1.getEmail().equals(email))
-                .findFirst();
-        if (patient.isEmpty()) {
-            return Optional.empty();
-        }
-        patients.remove(patient);
-        return patient;
+        return patientRepository.deletePatientByEmail(email);
     }
 
     public Optional<Patient> editPatient(String email, Patient patient) {
-        Optional<Patient> entity = patients.stream()
-                .filter(patient1 -> patient1.getEmail().equals(email))
-                .findFirst();
-        if (entity.isEmpty()) {
-            return Optional.empty();
-        }
-        entity.get().setBirthday(patient.getBirthday());
-        entity.get().setEmail(patient.getEmail());
-        entity.get().setPassword(patient.getPassword());
-        entity.get().setFirstName(patient.getFirstName());
-        entity.get().setLastName(patient.getLastName());
-        entity.get().setIdCardNo(patient.getIdCardNo());
-        entity.get().setPhoneNumber(patient.getPhoneNumber());
-        return Optional.of(patient);
+        return patientRepository.editPatient(email, patient);
     }
 
     public Optional<String> editPassword(String email, String password) {
-        Optional<Patient> patient = patients.stream()
-                .filter(patient1 -> patient1.getEmail().equals(email))
-                .findFirst();
-        if (patient.isEmpty()) {
-            return Optional.empty();
-        }
-        patient.get().setPassword(password);
-        return Optional.ofNullable(password);
+        return patientRepository.editPassword(email, password);
     }
 }
